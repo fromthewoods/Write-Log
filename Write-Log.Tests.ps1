fd1020e1c1ba2fb0b4799eb25c41841f2dcb1030
Joel Baltrus <jbaltrus@godaddy.com>
2016-05-26 11:18:41 -0700

    moving to verbose and better handling and more tests

--- Write-Log.Tests.ps1
+++ Write-Log.Tests.ps1
@@ -9,55 +9,95 @@ Import-Module $here\$moduleName.psd1 -Force
 
 
 
-Describe 'Write-Log' {
-    InModuleScope -ModuleName $moduleName {
-    
+InModuleScope -ModuleName $moduleName {
+    Describe 'Write-Log' {
+        It 'should exist' {
+            Get-Command -Name Write-Log\Write-Log -ErrorAction SilentlyContinue | Should Be $true
+        }
+        
         It 'creates C:\ERROR.log when Log is undefined' {
             $errorLog = 'C:\ERROR.log'
-            If (Test-Path $errorLog -ErrorAction SilentlyContinue)
-            {
-                Remove-Item -Path $errorLog -Force
+            If (Test-Path $errorLog -ErrorAction SilentlyContinue) {
+                Remove-Item -Path $errorLog
             }
             Test-Path $errorLog | Should Be $false
-            Write-Log -Message 'test message'
-            Test-Path $errorLog | Should Be $true
+            Write-Log -Message 'test message' -LogLevel 1
+            $errorLog | Should Contain 'test message'
         }
-
-        Context '#1' {
-            It "creates log directory if it doesn't exist" {
-                $LogDir = 'TestDrive:\dcinstall\Logs'
-                $LogFile  = Join-Path $LogDir 'test.log'
+        
+        Context 'No existing directory or log file' {
+            $LogDir = 'TestDrive:\dcinstall\Logs'
+            $LogFile  = Join-Path $LogDir 'test.log'
+            $PSDefaultParameterValues = @{ 'Write-Log:logFile' = $LogFile }
+                                           
+            It -Skip 'should not create a log file: LogLevel 0: Write-Host' {
                 Test-Path $LogDir | Should Be $false
-                Write-Log -Message 'test message' -LogFile $LogFile
+                Write-Log -Message 'Write-Host message' -LogLevel 0 | Should BeNullOrEmpty
+                Test-Path $LogDir | Should Be $false
+                Test-Path $LogFile | Should Be $false
+            }
+            
+            It 'should not create a log file: LogLevel 3: Write-Verbose -Verbose' {
+                Test-Path $LogDir | Should Be $false
+                #Write-Log -Message 'Write-Verbose message' -LogLevel 3 | Should BeNullOrEmpty
+                Write-Log -Message 'Write-Verbose message' -LogLevel 3 4>&1 | Should Match 'verbose'
+                Test-Path $LogDir | Should Be $false
+                Test-Path $LogFile | Should Be $false
+            }
+
+            It 'should create the log directory and file: LogLevel 1: Out-File' {
+                Test-Path $LogDir | Should Be $false
+                Write-Log -Message 'test message' -LogLevel 1 | Should BeNullOrEmpty
                 Test-Path $LogDir | Should Be $true
                 Test-Path $LogFile | Should Be $true
+                $LogFile | Should Contain 'test message'
             }
-        }
-
-        Context '#2' {
+            
+            # Creating function to fatten up the log file
             function New-TestFile {
                 param( [string]$FilePath,[int]$Size )
                 $i = 0
                 do {
-                    $i | Out-File -FilePath $FilePath -Append
+                    '0' | Out-File -FilePath $FilePath -Append
                     $i++
                 }
-                until ((Get-ChildItem $FilePath).Length -ge $Size)
+                until ((Get-ChildItem $FilePath).Length -ge $Size -or $i -gt 200)
+                #Microsoft.PowerShell.Utility\Write-Host $i
             }
 
-            It 'rolls over the log file after a certain size' {
-                $LogDir = 'TestDrive:\'
-                $LogFile  = Join-Path $LogDir 'test.log'
-                
-                # Create test file with garbage data to simulate size
-                New-TestFile -FilePath $LogFile -Size 1001
+            # update test file with garbage data to simulate size
+            New-TestFile -FilePath $LogFile -Size 1001
+            
+            It 'should roll over the log file after a certain size' {
                 Test-Path -Path $LogFile | Should Be $true
                 (Get-ChildItem -Path $LogDir).Count | Should Be 1
-
-                #The next call should rename the file and the following one will create a new log file.
-                Write-Log -Message 'this causes the file to be renamed' -MaxSize 1000 -LogFile $LogFile
-                Write-Log -Message 'this creates the new log file yayy' -MaxSize 1000 -LogFile $LogFile
+                Write-Log -Message 'this causes the file to roll over' -MaxSize 1000 -LogLevel 1
                 (Get-ChildItem -Path $LogDir).Count | Should Be 2
+                $LogFile | Should Contain 'this causes the file to roll over'
+            }
+        }
+        Context 'Existing directory and log file' {
+            $LogDir = 'TestDrive:\dcinstall\Logs'
+            $LogFile  = Join-Path $LogDir 'test.log'
+            $PSDefaultParameterValues = @{ 'Write-Log:logFile' = $LogFile }
+            
+            New-Item -Path $LogDir -ItemType Directory
+            Out-File -FilePath $LogFile -InputObject 'derp'
+            
+            It 'should append to the log file: LogLevel 1 (default)' {
+                $LogFile | Should Contain 'derp'
+                Write-Log -Message 'things' | Should BeNullOrEmpty
+            }
+            
+            It 'should accept -Verbose for the default loglevel' {
+                $LogFile | Should Not Contain 'verbosity'
+                Write-Log 'test verbosity' -Verbose 4>&1 | Should Match 'verbosity'
+                $LogFile | Should Contain 'verbosity'
+            }
+            
+            It 'should accept pipeline input' {
+                Write-Verbose 'verbioso' -Verbose 4>&1 | Write-Log 
+                $LogFile | Should Contain 'verbioso'
             }
         }
     }
--- Write-Log.psm1
+++ Write-Log.psm1
@@ -1,122 +1,115 @@
-function Write-Log
-{
-<#
-.Synopsis
-   Logs the passed "message" to a designated log file.
-.DESCRIPTION
-   Accepts a string input as a message and outputs it into a log file. If the log file
-   doesn't exist, it will create it. If the stamp param is passed it will prefix with
-   the time stamp. If debug mode is false it will skip writing to the log. If the log
-   gets bigger than 'maxsize' it will rename it and append the current date.
-.EXAMPLE
-   Write-Log "This is a message" -Stamp
-.PARAMETER Message
-    A string or array of strings that will be written to the log.
-.PARAMETER MaxSize
-    The maximum size in bytes that the log can be before the function rolls the log into a
-    new file.
-.PARAMETER LogLevel
-    Level 0 - Writes out to the screen via Write-Output
-    Level 1 - Writes out to a file
-    Level 2 - Writes to both the screen and file
-#>
+function Write-Log {
+    <#
+        .SYNOPSIS
+           Logs the passed "message" to a designated log file.
+        .DESCRIPTION
+           Accepts a string input as a message and outputs it into a log file. If the log file
+           doesn't exist, it will create it. If the stamp param is passed it will prefix with
+           the time stamp. If debug mode is false it will skip writing to the log. If the log
+           gets bigger than 'maxsize' it will rename it and append the current date.
+        .EXAMPLE
+           Write-Log "This is a message" -Stamp
+        .PARAMETER Message
+            A string or array of strings that will be written to the log.
+        .PARAMETER MaxSize
+            The maximum size in bytes that the log can be before the function rolls the log into a
+            new file.
+        .PARAMETER LogLevel
+            Level 0 - Writes out to the screen via Write-Host
+            Level 1 - Writes out to a file
+            Level 2 - Writes out to a file and to verbose
+            Level 3 - Writes out to verbose
+            Level 4 - Writes out to a file and to Write-Host
+    #>
 	[cmdletBinding()]
-	Param
-    (
+	Param (
         [Parameter(Position=0,
                    Mandatory=$true,
                    ValueFromPipeline=$true)]
         [ValidateNotNullOrEmpty()]
-        [string[]]$Message
-    ,
+        [string[]]$Message,
+        
         [Parameter(Position=1,
                    Mandatory=$false,
                    ValueFromPipeline=$false)]
-        [string]$LogFile = 'C:\ERROR.log'
-    ,
+        [string]$LogFile = 'C:\ERROR.log' ,
+        
         [Parameter(Position=2,
                    Mandatory=$false,
                    ValueFromPipeline=$false)]
-        [int]$MaxSize = 512000
-    ,
+        [int]$MaxSize = 512000,
+        
         [Parameter(Position=3,
                    Mandatory=$false,
                    ValueFromPipeline=$false)]
-        [ValidateSet('0','1','2')]
-        [int]$LogLevel = 0
+        [ValidateSet('0','1','2','3','4')]
+        [int]$LogLevel = 1
     )
     
-    Begin
-    {
-        Try
-        {
-            If ($LogFile -eq 'C:\ERROR.log')
-            {
-                $LogDir = 'C:\'
-                Write-Verbose "Logfile: $LogFile"
-                Write-Verbose "LogDir: $LogDir"
-            }
-            Else
-            {
-                $LogDir = $LogFile.Substring(0,$(($LogFile.LastIndexOf('\'))+1))
-                Write-Verbose "Logfile: $LogFile"
-                Write-Verbose "LogDir: $LogDir"
-            }
-            
-            If (!(Test-Path -Path $LogFile))
-            {
-                Write-Verbose "Part of the LogFile path doesn't exist."
-                If (!(Test-Path -Path $LogDir ))
-                {
-                    Write-Verbose "The LogDir doesn't exist."
-                    New-Item -Path $LogDir -ItemType Directory | Out-Null
+    Begin {
+        If ($LogLevel -ne 0 -and $LogLevel -ne 3) {
+            Try {
+                If ($LogFile -eq 'C:\ERROR.log') {
+                    $LogDir = 'C:\'
+                    Write-Debug "Logfile: $LogFile"
+                    Write-Debug "LogDir: $LogDir"
                 }
-                Write-Verbose "Log file does not exist. Creating..."
-                New-Item $LogFile -ItemType File | Out-Null
+                Else {
+                    $LogDir = $LogFile.Substring(0,$(($LogFile.LastIndexOf('\'))+1))
+                    Write-Debug "Logfile: $LogFile"
+                    Write-Debug "LogDir: $LogDir"
+                }
+                
+                If (!(Test-Path -Path $LogFile)) {
+                    Write-Debug "Part of the LogFile path doesn't exist."
+                    If (!(Test-Path -Path $LogDir )) {
+                        Write-Debug "The LogDir doesn't exist."
+                        New-Item -Path $LogDir -ItemType Directory | Out-Null
+                    }
+                    Write-Debug "Log file does not exist. Creating..."
+                    New-Item $LogFile -ItemType File | Out-Null
+                }
+                
+                # Roll the log over if it gets bigger than maxsize
+                $Log = Get-Item -Path $LogFile
+	            If ($Log.Length -gt $MaxSize) {
+                    Write-Debug "The LogFile is larger than $MaxSize bytes. Rolling over..."
+                    $date = Get-Date -UFormat %Y-%m-%d.%H-%M-%S
+                    $newName = $Log.BaseName + '__' + $date + $Log.Extension
+                    Write-Debug "Rolling log over because it reached maxsize: $MaxSize Bytes" | Out-File -FilePath $LogFile -Append
+		            Rename-Item -Path $LogFile -NewName $newName 
+	            }
             }
-            
-            # Roll the log over if it gets bigger than maxsize
-            $Log = Get-Item -Path $LogFile
-	        If ($Log.Length -gt $MaxSize)
-            {
-                Write-Verbose "The LogFile is larger than $MaxSize bytes. Rolling over..."
-                $date = Get-Date -UFormat %Y-%m-%d.%H-%M-%S
-                $newName = $Log.BaseName + '__' + $date + $Log.Extension
-                Write-Output "Rolling log over because it reached maxsize: $MaxSize Bytes" | Out-File -FilePath $LogFile -Append
-		        Rename-Item -Path $LogFile -NewName $newName 
-	        }
+            Catch {
+                $message = $_.Exception.Message
+                $position = $_.InvocationInfo.PositionMessage.Split('+')[0]
+                Write-Warning "ERROR: $message"
+                Write-Warning "ERROR: $position"
+                Throw "$message $position"
+            }
         }
-        Catch
-        {
-            Write-Error "ERROR: $($_.Exception.Message)"
-            Write-Error "ERROR: $($_.InvocationInfo.PositionMessage.Split('+')[0])"
-            Exit 1
-        }
-    }
-    Process
-    {
-        Foreach ($m in $Message)
-        {
+    }   
+    
+    Process {
+        Foreach ($m in $Message) {
             # Add the time stamp
             $m = "$(Get-Date) $m"
-            Try
-            {
-            	switch ($LogLevel)
-                {
+            Try {
+            	switch ($LogLevel) {
                     0 { Write-Host $m }
-                    1 { Out-File -InputObject $m -FilePath $LogFile -Append }
-                    2 { Out-File -InputObject $m -FilePath $LogFile -Append; Write-Host $m }
+                    1 { Out-File -InputObject $m -FilePath $LogFile -Append; Write-Verbose $m }
+                    2 { Out-File -InputObject $m -FilePath $LogFile -Append; Write-Verbose $m -Verbose }
+                    3 { Write-Verbose $m -Verbose }
+                    4 { Out-File -InputObject $m -FilePath $LogFile -Append; Write-Host $m }
                 }
             }
-            Catch
-            {
-                Write-Error "ERROR: $($_.Exception.Message)"
-                Write-Error "ERROR: $($_.InvocationInfo.PositionMessage.Split('+')[0])"
-                Exit 1
+            Catch {
+                $message = $_.Exception.Message
+                $position = $_.InvocationInfo.PositionMessage.Split('+')[0]
+                Write-Warning "ERROR: $message"
+                Write-Warning "ERROR: $position"
+                Throw "$message $position"
             } 
         }
-    }
-    End
-    {
     }
 }
